c3b4ccb3b225940edf60048d2029d08c1f6e7dea
farruhcool <54301929+farruhcool@users.noreply.github.com>
2020-10-05 11:44:26 +0500

    Update config.py

--- data/config.py
+++ data/config.py
@@ -1,21 +1,6 @@
-import os
-
-from dotenv import load_dotenv
-
-load_dotenv()
 
 BOT_TOKEN ="1256541898:AAE5-vJEBOSFJsLkN0QwhYB8EgXcyqS-yzQ"
 admins = [
     81039470
 ]
 
-ip = os.getenv("ip")
-
-aiogram_redis = {
-    'host': ip,
-}
-
-redis = {
-    'address': (ip, 6379),
-    'encoding': 'utf8'
-}
